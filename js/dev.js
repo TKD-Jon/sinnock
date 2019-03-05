2f0c696e631fbb651609bf80eac158fc87a1be69
TKD-Jon <46168629+TKD-Jon@users.noreply.github.com>
2019-03-05 13:50:19 -0600

    Fixing the mess up

---
+++ js/dev.js
@@ -0,0 +1,34 @@
+// NOTES
+// The click will uncheck before JS runs
+
+$(window).ready(function(){
+    updateparentHeight();
+    $('.checkbox').click(function(){
+        var thisLayer = $(this).attr('id');
+        checkIfActive(thisLayer);
+    })
+});
+
+function checkIfActive(thisLayer) {
+    var thisLayerID = '#'+thisLayer;
+    var thisLayerClass = '.'+thisLayer;
+
+    if ($(thisLayerID).is(':checked')) {
+        $(thisLayerClass).show(225);
+    } else {
+        $(thisLayerClass).hide(225);
+    }
+};
+
+function updateparentHeight() {
+    var totalTogglers = $('.main-layer-toggler-container').length;
+    
+    for (i = 1; i <= totalTogglers; i++) {
+        var parentID = "#layer-toggler-" + i;
+        var thisSelector = parentID +' .viewer > :nth-child(1)';
+        var childImageHeight = $(thisSelector).height();
+        $(parentID).css('height', childImageHeight)
+    };
+    
+    // $('.main-layer-toggler-container').css("height", childHeight)
+}
